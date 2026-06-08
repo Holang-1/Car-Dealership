513b30ab7616ecdaebdb48270443e140e35bd7aa
Holang_Khasibe <hokhacpt025@student.wethinkcode.co.za>
2026-06-08 12:12:59 +0200

    TEST: Added required tests

--- src/test/java/requiredTests/RequiredTest.java
+++ src/test/java/requiredTests/RequiredTest.java
@@ -5,4 +5,72 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RequiredTest {
+    @Test
+    void testInventoryCount() {
+        Dealership dealership = new Dealership();
+
+        dealership.addVehicle(
+                new Vehicle("Toyota", "Corolla", 250000)
+        );
+
+        dealership.addVehicle(
+                new Vehicle("Ford", "Ranger", 500000)
+        );
+
+        assertEquals(2, dealership.getInventoryCount());
+    }
+
+    @Test
+    void testInventoryValue() {
+        Dealership dealership = new Dealership();
+
+        dealership.addVehicle(
+                new Vehicle("Toyota", "Corolla", 250000)
+        );
+
+        dealership.addVehicle(
+                new Vehicle("Ford", "Ranger", 500000)
+        );
+
+        assertEquals(
+                750000,
+                dealership.getTotalInventoryValue()
+        );
+    }
+
+    @Test
+    void testMostExpensiveVehicle() {
+        Dealership dealership = new Dealership();
+
+        Vehicle v1 =
+                new Vehicle("Toyota","Corolla",250000);
+
+        Vehicle v2 =
+                new Vehicle("Ford","Ranger",500000);
+
+        dealership.addVehicle(v1);
+        dealership.addVehicle(v2);
+
+        assertEquals(
+                "Ford",
+                dealership.findMostExpensiveVehicle().getBrand()
+        );
+    }
+
+    @Test
+    void testVehicleInfo() {
+        Car car =
+                new Car(
+                        "Toyota",
+                        "Corolla",
+                        250000,
+                        4
+                );
+
+        assertEquals(
+                "Toyota Corolla - R250000.00 - 4 doors",
+                car.getVehicleInfo()
+        );
+    }
+
 }
