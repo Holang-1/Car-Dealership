a23c2d60c4e9e0769dc97be8053cf7ac5d81ec96
Holang_Khasibe <hokhacpt025@student.wethinkcode.co.za>
2026-06-08 12:08:30 +0200

    TEST: Added required test file

---
+++ src/test/java/requiredTests/RequiredTest.java
@@ -0,0 +1,8 @@
+package requiredTests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RequiredTest {
+}
