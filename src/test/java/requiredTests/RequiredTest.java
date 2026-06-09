2ae241709512fea1541ee62a52a43ed161bba4a9
Holang_Khasibe <hokhacpt025@student.wethinkcode.co.za>
2026-06-09 10:41:55 +0200

    FIX: Fixed tests

--- src/test/java/requiredTests/RequiredTest.java
+++ src/test/java/requiredTests/RequiredTest.java
@@ -1,75 +1,46 @@
 package requiredTests;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import com.holang_and_liyanda.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RequiredTest {
     @Test
     void testInventoryCount() {
-        Dealership dealership = new Dealership();
-
-        dealership.addVehicle(
-                new Vehicle("Toyota", "Corolla", 250000)
-        );
-
-        dealership.addVehicle(
-                new Vehicle("Ford", "Ranger", 500000)
-        );
+        CarDealershipApp dealership = new CarDealershipApp();
+        dealership.addVehicle(new Vehicle("Toyota", "Corolla", 250000, "CA-123-456"));
+        dealership.addVehicle(new Vehicle("Ford", "Ranger", 500000, "CA-132-465"));
 
         assertEquals(2, dealership.getInventoryCount());
     }
 
     @Test
     void testInventoryValue() {
-        Dealership dealership = new Dealership();
+        CarDealershipApp dealership = new CarDealershipApp();
+        dealership.addVehicle(new Vehicle("Toyota", "Corolla", 250000, "CA-465-312"));
+        dealership.addVehicle(new Vehicle("Ford", "Ranger", 500000, "CA-987-654"));
 
-        dealership.addVehicle(
-                new Vehicle("Toyota", "Corolla", 250000)
-        );
-
-        dealership.addVehicle(
-                new Vehicle("Ford", "Ranger", 500000)
-        );
-
-        assertEquals(
-                750000,
-                dealership.getTotalInventoryValue()
-        );
+        assertEquals(750000, dealership.getTotalInventoryValue());
     }
 
     @Test
     void testMostExpensiveVehicle() {
-        Dealership dealership = new Dealership();
-
-        Vehicle v1 =
-                new Vehicle("Toyota","Corolla",250000);
-
-        Vehicle v2 =
-                new Vehicle("Ford","Ranger",500000);
+        CarDealershipApp dealership = new CarDealershipApp();
+        Vehicle v1 = new Vehicle("Toyota","Corolla",250000, "CA-147-258");
+        Vehicle v2 = new Vehicle("Ford","Ranger",500000, "CA-741-852");
 
         dealership.addVehicle(v1);
         dealership.addVehicle(v2);
 
-        assertEquals(
-                "Ford",
-                dealership.findMostExpensiveVehicle().getBrand()
+        assertEquals("Ford", dealership.findMostExpensiveVehicle().getBrand()
         );
     }
 
     @Test
     void testVehicleInfo() {
-        Car car =
-                new Car(
-                        "Toyota",
-                        "Corolla",
-                        250000,
-                        4
-                );
+        Car car = new Car("Toyota", "Corolla", 250000, 4, "CA-963-369");
 
-        assertEquals(
-                "Toyota Corolla - R250000.00 - 4 doors",
-                car.getVehicleInfo()
+        assertEquals("Toyota Corolla - R250000.00 - 4 doors", car.getVehicleInfo()
         );
     }
 
